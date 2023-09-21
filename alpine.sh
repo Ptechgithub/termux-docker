2d50093a897c4048803d51d94c742f37d37c3527
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-09-21 21:17:42 +0330

    Add files via upload

--- alpine.sh
+++ alpine.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-clear
-echo "By 2--> Peyman * Github.com/Ptechgithub * "
-
 check_dependencies() {
-    local dependencies=("wget" "curl" "qemu-system-x86-64-headless" "qemu-utils")
+    local dependencies=("wget" "curl")
     
     for dep in "${dependencies[@]}"; do
         if ! command -v "${dep}" &> /dev/null; then
@@ -13,23 +10,32 @@ check_dependencies() {
         fi
     done
 }
+check_dependencies
 
 # Update the system and install required packages
-pkg update -y
-pkg upgrade -y
+pkg update && pkg upgrade -y
 pkg update
-check_dependencies
+pkg install qemu-system-x86-64-headless qemu-utils wget -y
 
 # Create a directory and download Alpine Linux ISO
 mkdir alpine && cd alpine
 wget https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-virt-3.18.3-x86_64.iso
 
-# Ask the user for storage size or use default (10GB)
-read -p "Enter the storage size in gigabytes (default: 10): " storage
-storage="${storage:-10}"
-qemu-img create -f qcow2 alpine.qcow2 "${storage}G"
+# Create a qcow2 file
+read -p "Enter the storage size in gigabytes (e.g., 10): " storage
+qemu-img create -f qcow2 alpine.qcow2 $storageG
 
-# Ask the user for RAM size or use default (512MB)
-read -p "Enter the RAM size in megabytes (default: 512): " ram_size
-ram_size="${ram_size:-512}"
-qemu-system-x86_64 -m "${ram_size}" -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -cdrom alpine-virt-3.18.3-x86_64.iso -nographic alpine.qcow2
+# Run the virtual machine
+read -p "Enter the  RAM size in megabytes (e.g., 512): " ram_size
+qemu-system-x86_64 -m 512 -netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 -nographic alpine.qcow2
+
+# Configure udhcpc settings
+mkdir -p /etc/udhcpc
+echo "RESOLV_CONF=\"no\"" > /etc/udhcpc/udhcpc.conf
+
+# Configure resolv.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+# Run initial Alpine Linux setup
+setup-alpine
